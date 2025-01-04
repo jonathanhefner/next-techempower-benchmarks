6aa2431fb3dcac39e020803010873baaafda6a8a
Jonathan Hefner <jonathan@hefner.pro>
2025-01-04 14:40:16 -0600

    Add type 4 (Fortunes) test

---
+++ app/fortunes/page.tsx
@@ -0,0 +1,30 @@
+import { db } from "@/lib/db"
+
+export default async function Page() {
+  const fortunes = await db.selectFrom("Fortune").selectAll().execute()
+
+  fortunes.push({ id: 0, message: "Additional fortune added at request time." })
+
+  fortunes.sort((a, b) => a.message.localeCompare(b.message))
+
+  return <>
+    <title>Fortunes</title>
+
+    <table>
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>message</th>
+        </tr>
+      </thead>
+      <tbody>
+        {fortunes.map(fortune =>
+          <tr key={fortune.id}>
+            <td>{fortune.id}</td>
+            <td>{fortune.message}</td>
+          </tr>
+        )}
+      </tbody>
+    </table>
+  </>
+}
--- lib/schema.ts
+++ lib/schema.ts
@@ -2,6 +2,7 @@ import { Generated, Selectable } from "kysely"
 
 export interface Database {
   World: WorldTable
+  Fortune: FortuneTable
 }
 
 export interface WorldTable {
@@ -10,3 +11,10 @@ export interface WorldTable {
 }
 
 export type World = Selectable<WorldTable>
+
+export interface FortuneTable {
+  id: Generated<number>
+  message: string
+}
+
+export type Fortune = Selectable<FortuneTable>
